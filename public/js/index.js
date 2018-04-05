6e5db0475ac6480f87203ad477718f65ca0fce46
Anshika Singh <anshika135singh@gmail.com>
2018-04-04 22:52:06 -0500

    Add CSS style to the App

--- public/js/index.js
+++ public/js/index.js
@@ -25,14 +25,16 @@ socket.on('newLocationMessage', function(newMessage) {
 	$('#messages').append(li);
 });
 
+var messageTextBox = $('[name = message]');
+
 $('#message-form').on('submit', function(e){
 	e.preventDefault();
 
 	socket.emit('createMessage', {
 		'from' : 'User',
-		'text' : $('[name = message]').val()
-	}, function(data) {
-		
+		'text' : messageTextBox.val()
+	}, function() {
+		messageTextBox.val('');
 	});
 });
 
@@ -43,12 +45,15 @@ locationButton.on('click', function(){
 		return alert('Geolocation not supported by your browser');
 	}
 
+	locationButton.attr('disabled','disabled').text('Sending Location...');
 	navigator.geolocation.getCurrentPosition( function(position) {
+		locationButton.removeAttr('disabled').text('Send Location');
 		socket.emit('createLocationMessage', {
 			'latitude' : position.coords.latitude,
 			'longitude' : position.coords.longitude
 		});
 	}, function() {
+		locationButton.removeAttr('disabled').text('Send Location');
 		alert('Unable to fetch geolocation');
 	});
 });
