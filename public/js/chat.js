e844e9bb13d7ea8b7c8993eefb7fb6582a86d1e8
Anshika Singh <anshika135singh@gmail.com>
2018-04-06 13:19:11 -0500

    Send messages only to people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -72,7 +72,6 @@ $('#message-form').on('submit', function(e){
 	e.preventDefault();
 
 	socket.emit('createMessage', {
-		'from' : 'User',
 		'text' : messageTextBox.val()
 	}, function() {
 		messageTextBox.val('');
